ed9e02e3b14a623a432210329c889df1bf4eecec
AjayHajare <30743629+AjayHajare@users.noreply.github.com>
2018-09-19 18:03:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+var geca=new Object();
+	geca.city="aurangabad";
+	geca.college="government college of engineering, aurangabad";
+	geca.address="usamanpura,aurangabad";
+	geca.web="geca.co.in";
+	geca.fees=60000;
+	geca.cet=123;
+	
+var pict=new Object();
+	pict.city="pune";
+	pict.college="pune institute of computer technology";
+	pict.address="swargate,pune";
+	pict.web="pict.co.in";
+	pict.fees=100000;
+	pict.cet=145;
+	
+var coep=new Object();
+	coep.city="pune";
+	coep.college="college of engineering pune";
+	coep.address="shivajinagar,pune";
+	coep.web="coep.co.in";
+	coep.fees=70000;
+	coep.cet=169;
+
+var mitp=new Object();
+	mitp.city="pune";
+	mitp.college="maharshtra institute of technology,pune";
+	mitp.address="kjsajk,pune";
+	mitp.web="mitp.co.in";
+	mitp.fees=140000;
+	mitp.cet=120;
+	
+var mita=new Object();
+	mita.city="aurangabad";
+	mita.college="maharashtra institute 0f technology,aurangabad";
+	mita.address="satara parisar";
+	mita.web="mita.co.in";
+	mita.fees=90000;
+	mita.cet=100;
+var jnec=new Object();
+	jnec.city="aurangabad";
+	jnec.address="shivaji nagar";
+	jnec.college="javaharlal neharu college of engineering,aurangabad";
+	jnec.web="jnec.co.in";
+	jnec.fees=70000;
+	jnec.cet=90;
+	
+var karad=new Object();
+	karad.city="satara";
+	karad.address="karad,satara";
+	karad.college="government college of karad";
+	karad.web="karad.co.in";
+	karad.fees=60000;
+	karad.cet=120;
+var asssms=new Object();
+	asssms.city="satara";
+	asssms.address="satra,satara";
+	asssms.college="shahu institute of technology";
+	asssms.web="asssms.web";
+	asssms.fees=50000;
+	asssms.cet=59;
